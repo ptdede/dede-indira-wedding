6ec42f548053face4c826bd566b2e3632fe5939e
Dede Adhyatmika <macbook-putugede@ptdede.local>
2023-08-25 01:53:48 +0800

    fix: bring back font

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,5 @@
+import "@fontsource/bodoni-moda/400.css";
+import "@fontsource/bodoni-moda/700.css";
 import "@fontsource/montserrat/300.css";
 import "@fontsource/montserrat/400.css";
 import "@fontsource/montserrat/700.css";
